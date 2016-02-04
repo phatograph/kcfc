c94cf4708daf509716bf4429fd11086017172f91
Phat Wangrungarun <phatograph@gmail.com>
2016-02-04 01:50:00 +0000

    Add GH ribbon

--- app/app.js
+++ app/app.js
@@ -31,7 +31,7 @@ let App = React.createClass({
           {this.state.data.map((d, i) => {
             return (
               <li key={i}>
-                <a href={d.url}>
+                <a href={d.url} target="_blank">
                   <div className={style.img}>
                     <img src={d.img} alt="img" />
                   </div>
@@ -48,6 +48,7 @@ let App = React.createClass({
         <footer>
           2016 &copy; phatograph.com
         </footer>
+        <a className={style.gh} href="https://github.com/phatograph/kcfc" target="_blank"><img src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" /></a>
       </div>
     );
   }
